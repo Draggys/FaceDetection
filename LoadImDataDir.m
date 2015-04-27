3fd2a27eb894aa361ab933f76742bf9f4b90690e
Kim <draggys@gmail.com>
2015-04-27 21:41:24 +0200

    Load all images in a directory

---
+++ LoadImDataDir.m
@@ -0,0 +1,20 @@
+function [ ii_ims ] = LoadImDataDir( dirname, varargin )
+%LoadImDataDir load images from a directory
+    % Input: 
+        % dirname, path to dir containing images
+        % optional varargin, specify #images to load
+    % Output: Matrix of integral images stored as columns
+    
+    images = dir([dirname '*.bmp']);
+    len = size(images, 1);
+    
+    if nargin > 1
+       len = varargin{1};
+    end
+    
+    for i = 1 : len
+        [im, ii_im] = LoadIm(images(i).name);
+        ii_ims(:, i) = ii_im(:);
+    end   
+end
+
