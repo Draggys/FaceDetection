cfbb13a0076c0141876943fcdedc8157d1f9fa71
Kim <draggys@gmail.com>
2015-04-27 18:53:40 +0200

    VecAllFeatures

---
+++ VecAllFeatures.m
@@ -0,0 +1,13 @@
+function [ fmat ] = VecAllFeatures( all_ftypes, W, H )
+% VecAllFeatures computes a feature matrix
+    % Input: all_ftypes, matrix with features, where a row is a feature
+       % W, H are the dimensions of the image
+    % Output: fmat of size nf x W*H where each row is a feature
+    
+    fmat = zeros(size(all_ftypes, 1), W * H);
+    for i = 1 : size(all_ftypes, 1)
+        fmat(i, :) = VecFeature(all_ftypes(i, :), W, H);
+    end
+
+end
+
