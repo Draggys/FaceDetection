47e45cef988dfef492016c59056f0c24565e94d8
Kim <draggys@gmail.com>
2015-04-29 21:49:42 +0200

    Added two new variables to save
    nrFaces and nrNFaces

--- SaveTrainingData.m
+++ SaveTrainingData.m
@@ -13,12 +13,15 @@ function [ ] = SaveTrainingData( all_ftypes, train_inds, s_fn )
     faces = LoadImDataDir('Data\TrainingImages\FACES\');
     nfaces = LoadImDataDir('Data\TrainingImages\NFACES\');
     ii_ims = [faces nfaces];
-    
+   
     % Ground truth array
     ys = [ones(1, size(faces, 2)) -ones(1, size(nfaces, 2))];
     
     fmat = VecAllFeatures(all_ftypes, W, H);
+   
+    nrFaces = size(faces, 2);
+    nrNFaces = size(nfaces, 2);
     
-    save(s_fn, 'ii_ims', 'ys', 'fmat', 'all_ftypes', 'W', 'H');
+    save(s_fn, 'ii_ims', 'ys', 'fmat', 'all_ftypes', 'W', 'H', 'nrFaces', 'nrNFaces');
 end
 
